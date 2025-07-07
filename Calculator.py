4b5b8ebf4d10856e477fe77728eb68ba2ce43c5a
franckyhaba <franckyhaba75@gmail.com>
2025-07-07 20:10:49 +0100

    I added error handlers for the first part of the code which will loop if a string or an invalid value is added build on this progress.

--- Calculator.py
+++ Calculator.py
@@ -3,13 +3,25 @@ Msg = ("Welcome to franckys Calculator!")
 print(Msg) 
 print ()
 
+while True:
+    try:  
+        firstNum= int(input ("Enter first number: "))
+        break # Exit loop if input is valid
+    except ValueError:
+        print("")
+        print("Invalid input, please enter a number.")  
+        print("")
+while True:
+    try:  
+        secondNum = int (input("Enter second number: "))
+        break # Exit loop if input is valid
+    except ValueError:
+        print("Invalid input, please enter a number.") 
 
-firstNum= int(input ("Enter first number: "))
-secondNum = int (input("Enter second number: "))
 operator= input("Enter operator (+, -, *, /): ")
+print("")
 
-
-if operator == '+':
+if operator == '+': 
     result = (firstNum + secondNum)
 elif operator == "-":
     result = (firstNum- secondNum)
@@ -18,7 +30,14 @@ elif operator == "*":
 elif operator == "/":
     result = (firstNum / secondNum)
 else: 
+    print("")
     result = "Invalid operator, please use +, -, *, or /"
 
 print("The result is: ", result)
-
+# End of Calculator.py
+print("")
+print("Thank you for using franckys Calculator!")
+print("")
+print("Goodbye!")
+print()
+# End of program
