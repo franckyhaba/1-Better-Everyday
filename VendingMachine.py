8e423436f2f49029bfc4ff742b5e12a1fbf20d12
franckyhaba <franckyhaba75@gmail.com>
2025-07-09 21:36:38 +0100

    added a balance

--- VendingMachine.py
+++ VendingMachine.py
@@ -2,6 +2,8 @@ msg = "Welcome to Franckys Vending Machine, select what you would like:"
 print(msg)
 print()  # This creates a blank line for better spacing
 
+balance = 5.00
+
 while True:
     try:
         option = int(input("1. Water, 2. KitKat, 3. Coke, 4. Exit: "))
@@ -12,14 +14,18 @@ while True:
     if option == 1:
         price = 2.00
         print(f"You have selected Water, and it costs £{price:.2f}\n")
-        
+        balance -= price 
+        print (balance) 
     elif option == 2:
         price = 1.50
         print(f"You have selected KitKat, and it costs £{price:.2f}\n")
-        
+        balance -= price 
+        print (balance) 
     elif option == 3:
         price = 2.50
         print(f"You have selected Coke, and it costs £{price:.2f}\n")
+        balance -= price 
+        print (balance) 
         
     elif option == 4:
         print("Thank you for using Franckys Vending Machine. Have a good day!\n")
