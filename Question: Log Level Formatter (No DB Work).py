9b2424567fdfb2a2bda564a7eaa8c3634e3c2141
franckyhaba <franckyhaba75@gmail.com>
2025-07-15 19:08:48 +0100

    updated the code and also working

--- Question: Log Level Formatter (No DB Work).py
+++ Question: Log Level Formatter (No DB Work).py
@@ -3,22 +3,23 @@ def LogLvl ():
     while True :
         
         try:
-            Keyword = str(input("Enter log level: " )).upper()
-            print (Keyword)
+            Keyword = input("Enter log level: " ).upper()
+            print (f'User entered {Keyword}')
         
         except ValueError: 
             print('Please enter a Log level!')
             continue 
         
-        for Error in Keyword:
+        if  Keyword == "Error":
             print(f'{Keyword} has been logged a error')
             
-        if Keyword == "Warning":
+        elif Keyword == "Warning":
             print(f'{Keyword} has been logged a Warning')
             
         elif Keyword == "info": 
             print(f'{Keyword} has been logged a info')
         else:
-            print(f'{Keyword} No key word can be found')
+            print(f'{Keyword} No key word can be found')    
+        break 
 
 LogLvl()
