6edf2216791aea48ca200c643aa564394731c4ab
franckyhaba <franckyhaba75@gmail.com>
2025-07-15 18:23:53 +0100

    didnt call log

---
+++ Question: Log Level Formatter (No DB Work).py
@@ -0,0 +1,24 @@
+def LogLvl ():
+    
+    while True :
+        
+        try:
+            Keyword = str(input("Enter log level: " )).upper
+            print (Keyword)
+        
+        except ValueError: 
+            print('Please enter a Log level!')
+            continue 
+        
+        for Error in Keyword:
+            print(f'{Keyword} has been logged a error')
+            
+        if Keyword == "Warning":
+            print(f'{Keyword} has been logged a Warning')
+            
+        elif Keyword == "info": 
+            print(f'{Keyword} has been logged a info')
+        else:
+            print(f'{Keyword} No key word can be found')
+
+LogLvl()
