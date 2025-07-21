af2bb8cb7d29d6fdb00ea0ee0c3b77d301313b7e
franckhaba <dufferking10@gmail.com>
2025-07-21 21:15:36 +0100

    addind structure and morefixing while loop

--- Email Classifier.py
+++ Email Classifier.py
@@ -4,23 +4,24 @@ def emailClassifier() :
     while True:
             email = str(input('Please input your email so we can classify it in system:"eg. info@Pyexll.com"....   '))
             print(email)
-            if "@" not in email:
+            if  "@" not in email or ".com" not in email:
+
              print(f'Your {email} is missing an "@".')
              continue
-            elif "@" in email:
-                break
-            
-            
-            if email == 'someone@gmail.com': 
-                print(f'Your {email} has been sorted in the personal email sections.')
+         
+            else:
                 
-            elif email == 'junkuser@random.com':
-                print(f'Your {email} has been sorted in the junk email sections.')
             
-            elif email == 'email1@company.com':
-                print(f'Your {email} has been sorted in the work email sections.')
+                if email == 'someone@gmail.com': 
+                    print(f'Your {email} has been sorted in the personal email sections.')
                     
+                elif email == 'junkuser@random.com':
+                    print(f'Your {email} has been sorted in the junk email sections.')
                 
+                elif email == 'email1@company.com':
+                    print(f'Your {email} has been sorted in the work email sections.')
+                        
+                    
                 
          
 emailClassifier()
