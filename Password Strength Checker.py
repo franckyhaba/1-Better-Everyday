4d79a9caa5e62bb5b7081d9878bc0b6ed736ee1f
franckhaba <dufferking10@gmail.com>
2025-08-04 07:59:09 -0400

    alot to learn from this code things like char.isdigit whih i never used

--- Password Strength Checker.py
+++ Password Strength Checker.py
@@ -24,40 +24,28 @@ print(passRules)
 
 reqPass = input('Please input password password must follow the rules listed above: ')
 
-def checkPasswordStrength(reqPass) :
-   while True:  
-                
-    hasNum = any(char.isdigit() for char in reqPass)
-    hasUpper= any(char.isupper() for char in reqPass)         
-    hasLower = any(char.islower() for char in reqPass)
-    hasSpecial = any(char in string.punctuation for char in reqPass)           
-    longNum = len(reqPass)>= 8                     
-                
-    if hasNum and hasUpper and hasLower and hasSpecial and longNum:
-        print("Password has been accepted ")
-        break
-     
-    else: 
-        print("Password is missing: ")
-        if not hasNum: 
-         print("- number ")    
-         
-        print("Password is missing: ")
-        if not hasUpper: 
-         print("- Upper case ")    
-         
-        print("Password is missing: ")
-        if not hasLower: 
-         print("- Lower Case")    
-          
-        print("Password is missing: ")
-        if not hasSpecial: 
-         print("- special character ")    
-          
-     
-        if not longNum: 
-         print("Password is not long enough")    
-         
-    
-    
-   
+def checkPasswordStrength(reqPass):
+    while True:
+        hasNum = any(char.isdigit() for char in reqPass)
+        hasUpper = any(char.isupper() for char in reqPass)
+        hasLower = any(char.islower() for char in reqPass)
+        hasSpecial = any(char in string.punctuation for char in reqPass)
+        longNum = len(reqPass) >= 8
+
+        if hasNum and hasUpper and hasLower and hasSpecial and longNum:
+            print("Password has been accepted ")
+            break
+        else:
+            print("Password is missing:")
+            if not hasNum:
+                print("- number")
+            if not hasUpper:
+                print("- Upper case")
+            if not hasLower:
+                print("- Lower Case")
+            if not hasSpecial:
+                print("- special character")
+            if not longNum:
+                print("Password is not long enough")
+            reqPass = input('Please input password password must follow the rules listed above: ')
+checkPasswordStrength(reqPass)
