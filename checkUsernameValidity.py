b317312d71373dbb3085d669303d8965b2353450
franckhaba <dufferking10@gmail.com>
2025-08-04 21:37:26 -0400

    added new instruction for check user password

---
+++ checkUsernameValidity.py
@@ -0,0 +1,10 @@
+'''Asks the user to input a username.
+Keeps asking until the username meets the following rules:
+Must be at least 6 characters long.
+Must start with a letter.
+Must only contain letters, numbers, or underscores.
+Must not contain any spaces.
+If the username doesn't meet the criteria, print what it’s missing.
+
+Once a valid username is entered, print "Username accepted." and exit the loop.
+'''
