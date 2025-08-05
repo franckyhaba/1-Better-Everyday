719268ca523799230f1e0123754e6ea2206db4a5
franckhaba <dufferking10@gmail.com>
2025-08-04 22:18:54 -0400

    added a loop

--- checkUsernameValidity.py
+++ checkUsernameValidity.py
@@ -8,6 +8,7 @@ If the username doesn't meet the criteria, print what it’s missing.
 
 Once a valid username is entered, print "Username accepted." and exit the loop.
 '''
+import string 
 msg = ( '''Asks the user to input a username.
 Keeps asking until the username meets the following rules:
 Must be at least 6 characters long.
@@ -18,24 +19,36 @@ If the username doesn't meet the criteria, print what it’s missing.''')
 print(msg)
 print
 
-UserName =input('Please input username: ')
+userName = input('Please input username: ')
 
-def checkUsernameValidity(UserName):
+def checkUsernameValidity(userName):
     
+    while True:
+        longerSix = len(userName) >= 6 
+        hasLetter = any(char.isstring() for char in userName)
+        hasSpecial = any(char in string.punctuation and char.isdigit for char in userName)
+        hasSpace = any(char .isspace() for char in userName)
+        
+        if longerSix and hasLetter and hasSpecial and hasSpace:
+            print(f'{userName} has been been accepted')
+            break 
+        else:
+            
+            if not longerSix:
+                print (f'{userName }is not longer then 6 characters.')
+             
+            if not hasLetter: 
+                print (f'{userName } does not begin with a letter.')
+            
+            if not hasSpecial: 
+                print (f'{userName }has no special characters.')
+            
+            if not hasSpace: 
+                print (f'{userName } has a space please get rid of it.')
+                
+            print(msg)
+            print 
+            userName = input('Please follow the rules when picking a username')  
+             
     
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    checkUsernameValidity(UserName)
+checkUsernameValidity(userName)
