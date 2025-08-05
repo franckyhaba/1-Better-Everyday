3952e39e8e2212d25043ef56a08471c92c0079f2
franckhaba <dufferking10@gmail.com>
2025-08-04 21:41:50 -0400

    done def functions

--- checkUsernameValidity.py
+++ checkUsernameValidity.py
@@ -8,3 +8,34 @@ If the username doesn't meet the criteria, print what it’s missing.
 
 Once a valid username is entered, print "Username accepted." and exit the loop.
 '''
+msg = ( '''Asks the user to input a username.
+Keeps asking until the username meets the following rules:
+Must be at least 6 characters long.
+Must start with a letter.
+Must only contain letters, numbers, or underscores.
+Must not contain any spaces.
+If the username doesn't meet the criteria, print what it’s missing.''')
+print(msg)
+print
+
+UserName =input('Please input username: ')
+
+def checkUsernameValidity(UserName):
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    checkUsernameValidity(UserName)
