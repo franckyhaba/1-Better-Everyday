63368f1431d4ee1a57cedfa80eb5722c782c0bd0
franckhaba <dufferking10@gmail.com>
2025-08-07 17:38:58 -0400

    code complete

--- LeetcodeQ/twoSum.py
+++ LeetcodeQ/twoSum.py
@@ -1,9 +1,11 @@
 def twoSum (nums, target):
-    
+    pairs = []
     for i in range (len(nums)):
         for j in range (i + 1, (len(nums))):
             if nums[i] + nums [j] == target:
-                return [i,j]
+                pairs.append((i,j))
+                
+    return pairs
         
         
 result = twoSum([1,2,3,2,4],5)
