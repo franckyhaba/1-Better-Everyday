5875683b9b056c2758ae4aa1d3bf879bd0ef2398
franckhaba <dufferking10@gmail.com>
2025-09-28 20:26:51 +0100

    done cube

--- PythonCrashCourse/Counting.py
+++ PythonCrashCourse/Counting.py
@@ -1,10 +1,12 @@
 oddNumbers =[]
 
-for values in range (3,31,3):
-    oddNumber = values
+for values in range (1,11):
+    oddNumber = values ** 3
     oddNumbers.append(oddNumber)
 print(oddNumbers)
+
 #print(values)
 
 #oddNumbers= list(range(3,21,3))
 #print (oddNumbers)
+
