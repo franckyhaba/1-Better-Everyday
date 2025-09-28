57cb1751a0b6da8f882fbc07c59887fd07f72cd9
franckhaba <dufferking10@gmail.com>
2025-09-28 19:30:37 +0100

    no 1-21 use of range

--- FewBits/Pizza.py
+++ FewBits/Pizza.py
@@ -5,4 +5,5 @@ pizzas = ["cheese pizza", "pepperoni pizza", "meat feast pizza"]
 
 for pizza in pizzas:
     print(f"I like {pizza.title()}!")
-print(f"We will get the following pizza for the party: {pizza.title()}.\n")
+    print(f"We will get the following pizza for the party: {pizza.title()}.\n")
+
