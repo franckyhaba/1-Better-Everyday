d6d6178f429e3757ddb755ad8298e85a1cad5fcd
franckhaba <dufferking10@gmail.com>
2025-09-29 21:43:03 +0100

    done the chatgpt questions

--- PythonCrashCourse/counting2.py
+++ PythonCrashCourse/counting2.py
@@ -1,6 +1,37 @@
-numbers = []
+# numbers = []
 
-for values in range(1,11):
-    number = values * 5
-    numbers.append(number)
-print (numbers)
+# for values in range(1,11):
+#     number = values * 5
+#     numbers.append(number)
+# print (numbers)
+
+# squares = []
+
+# for values in range (1,11):
+#     square = values ** 2
+#     squares.append(square)
+# print(squares)
+
+# evenNumbers =[ ]
+# for values in range (2,21,2):
+#      evenNumber =values 
+#      evenNumbers.append(evenNumber)
+# print (evenNumbers)
+
+
+# numbers = []
+# #list(range(1,101))
+
+# for values in range (1,101,1):
+#     number = values ** 2
+#     numbers.append(number)     
+# print(min(numbers))
+
+# print(max(numbers))
+
+# print(sum(numbers))
+
+Numbers =[values ** 2 for values in range (1,11)] 
+print(Numbers)
+
+
