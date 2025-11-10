c0e807d60eecc674a8b2a96f6ec32dfd711a0c03
franckhaba <dufferking10@gmail.com>
2025-11-10 23:00:31 +0000

    added the same lines for events ad also transport

--- ExpenseAnalyser.py
+++ ExpenseAnalyser.py
@@ -3,9 +3,11 @@ print("Welcome to your simple expense analyzer!\n")
 MonthlyLimits = (500,350,500,) #food, transport, events 
 
 foodExpenses = []
-transportExpense = []
-eventsExpense = []
+transportExpenses = []
+eventsExpenses = []
 
 print("Please input your expense details: ")
 
-foodExpense.append(float(input('Money spend on food (£): ')))
+foodExpenses.append(float(input('Money spend on food (£): ')))
+transportExpenses.append(float(input('Money spend on transport (£): ')))
+eventsExpenses.append(float(input('Money spend on events (£): ')))
