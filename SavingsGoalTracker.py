46efaf3c5579a9e6d68543d141c2de733651b563
franckhaba <dufferking10@gmail.com>
2025-11-11 12:17:54 +0000

    doing a new one questions, savings goals tracker, and done the tuples

---
+++ SavingsGoalTracker.py
@@ -0,0 +1,10 @@
+print = ('=== Welcome to the saving goal tracker! ===')
+
+
+categories = (
+    
+    200 #emergency       
+    ,300#fund
+    ,500 #travel 
+    ,1000
+    ) #emergency fund, travel fund, gadget fund)
