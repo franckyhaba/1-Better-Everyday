6db0d292d48204b1e0a9a186176ba886dc72636f
franckhaba <dufferking10@gmail.com>
2025-11-11 12:56:34 +0000

    list object attribute error now come back and fix and also i printed the differnt saving per month and i want to append list to store the monthly spend

--- SavingsGoalTracker.py
+++ SavingsGoalTracker.py
@@ -1,10 +1,24 @@
 print = ('=== Welcome to the saving goal tracker! ===')
 
 
-categories = (
+savingTotals = (
     
     200 #emergency       
-    ,300#fund
+    ,300# mutual 
     ,500 #travel 
-    ,1000
-    ) #emergency fund, travel fund, gadget fund)
+    ,1000 #device
+    ) #emergency fund, mutual, travel fund, device fund
+
+emergencySaving= [0]
+mutualSaving= [1]
+travelSaving =  [2]
+deviceSaving= [3]
+
+print = ("Please enter the saving for the month: ")
+emergencySaving.append = float(input("Emergency savings for this month(£):  "))
+mutualSaving.append = float(input("Mutual savings for this month(£):  "))
+travelSaving.append = float(input("Travel savings for this month(£):  "))
+deviceSaving.append = float(input("Device savings for this month(£):  "))
+
+
+
