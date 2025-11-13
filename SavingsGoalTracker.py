028e7064ddd17556a6fe48ebaf752610732f2a69
franckhaba <dufferking10@gmail.com>
2025-11-13 14:54:32 +0000

    change the print statments

--- SavingsGoalTracker.py
+++ SavingsGoalTracker.py
@@ -1,4 +1,4 @@
-print = ('=== Welcome to the saving goal tracker! ===')
+print  ('\n=== Welcome to the saving goal tracker! ===\n')
 
 
 savingTotals = (
@@ -14,12 +14,24 @@ mutualSaving= []
 travelSaving =  []
 deviceSaving= []
 
-print = ("Please enter the saving for the month: ")
+print("Please enter the saving for the month: \n")
 emergencySaving.append (float(input("Emergency savings for this month(£):  ")))
 mutualSaving.append (float(input("Mutual savings for this month(£):  ")))
 travelSaving.append (float(input("Travel savings for this month(£):  ")))
 deviceSaving.append (float(input("Device savings for this month(£):  ")))
 
+#calc so now i want to calc and show the total and the limit.
+
+totalEmergencySavings = sum (emergencySaving)
+totalMutualSavings = sum (mutualSaving)
+totalTravelSavings = sum (travelSaving)
+totalDeviceSavings = sum (deviceSaving)
+
+print ("==== Monthly budget====")
+print (f"Emergency: £{totalEmergencySavings}/{savingTotals[0]}")
+print (f"Mutual: £{totalMutualSavings}/{savingTotals[1]}")
+print (f"Travel: £{totalTravelSavings}/{savingTotals[2]}")
+print (f"Device: £{totalDeviceSavings}/{savingTotals[3]}")
 
 
 
