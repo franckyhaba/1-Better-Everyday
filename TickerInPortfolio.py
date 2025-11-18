e225ff53c5ac7dbbb45cf3cfb2b8e83c51f8acec
franckhaba <dufferking10@gmail.com>
2025-11-18 22:07:43 +0000

    added a append list

--- TickerInPortfolio.py
+++ TickerInPortfolio.py
@@ -4,11 +4,12 @@ tickers = ["MC fp", "RI fp","BNP fp", "OR fp", "CA fp", "RNO fp"]
 for ticker in tickers:
     addTicker = (input("\n Enter the ticker you want to the portfolio: "))
     
-    if addTicker not in tickers: 
+    if addTicker in tickers: 
         print ("This tickers is already in the portfolio try again! ")
         
-    elif addTicker in tickers:
+    elif addTicker not in tickers:
         print ("The ticker has been added to the portfolio! ")
+        tickers.append(addTicker)
         print(tickers)
         break 
             
