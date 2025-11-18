e073aa12b40b89034779033ec6daa126b70c79f0
franckhaba <dufferking10@gmail.com>
2025-11-18 21:53:10 +0000

    dont all the code but didnt check to see if theres errors

--- TickerInPortfolio.py
+++ TickerInPortfolio.py
@@ -1,3 +1,13 @@
+# list of tickers 
 tickers = ["MC fp", "RI fp","BNP fp", "OR fp", "CA fp", "RNO fp"]
 
-#
+for ticker in tickers:
+    addTicker = (input("Enter the ticker you want to the portfolio: "))
+    addTicker == tickers 
+    print ("This tickers is already in the portfolio try again! ")
+
+    if  addTicker != tickers:
+        print ("The ticker has been added to the portfolio! ")
+        print(tickers)
+        break 
+            
