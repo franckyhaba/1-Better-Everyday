d078fc3c5a5a2de7e61d946894bdd2e2fbb6e923
franckhaba <dufferking10@gmail.com>
2025-11-19 10:56:03 +0000

    added a for loop

--- MonthlyExpense.py
+++ MonthlyExpense.py
@@ -8,6 +8,11 @@ monthlyExpenses = {
 instructions= ('category available "Rent", "Food", "Transport", "Entertainment"')
 print(instructions)
 
-chooseCat = input('\n Please choose from the category available "Rent", "Food", "Transport", "Entertainment": ')
-chooseCat.title()
+for monthlyExpense in monthlyExpenses :
+    chooseCat = input('\n Please choose from the category available "Rent", "Food", "Transport", "Entertainment": ')
+    chooseCat.title()
+
+    if chooseCat in monthlyExpenses: 
     
+     msg = input(f"\n The category you have selected is {chooseCat}, PLease enter the amount spent this month:  ")
+     print (msg)
