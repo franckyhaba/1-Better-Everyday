6f0542a23e764db3087d028556118e08f2eb5559
franckhaba <dufferking10@gmail.com>
2025-11-19 10:35:46 +0000

    added a new program  monthly expenses and done the base of the code store the expenses etc and doing the input part

---
+++ MonthlyExpense.py
@@ -0,0 +1,11 @@
+monthlyExpenses = { 
+                  'Rent': 500,
+                  'Food': 60,
+                  'Transport': 120,
+                  'Fun': 80
+                  }
+monthlyExpenses.lower
+instructions= ('category available "Rent", "Food", "Transport", "Entertainment"')
+print(instructions)
+
+chooseCat = input('\n Please choose from the category available "Rent", "Food", "Transport", "Entertainment": ')    
