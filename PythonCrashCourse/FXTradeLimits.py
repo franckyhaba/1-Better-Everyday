0a120d254bc6ab47a5ea11e906f641855d51e4e6
franckhaba <dufferking10@gmail.com>
2025-11-22 14:03:46 +0000

    allowing user to input the currency they want to trade in

--- PythonCrashCourse/FXTradeLimits.py
+++ PythonCrashCourse/FXTradeLimits.py
@@ -8,5 +8,8 @@ Currency = {
 
 
 print(Intro)
+currencyTrade = "\n Please select the currency you want to trade in:  "
+print(currencyTrade)
+
 while True :
     trade = float(input("\n Enter FX trade size you want to make in £ "))
