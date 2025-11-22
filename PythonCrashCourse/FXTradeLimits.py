37f6dbb1d8f5d692ed32141a7891fdc9771c02e9
franckhaba <dufferking10@gmail.com>
2025-11-22 14:00:41 +0000

    doing a prorgram that calcs, fx trade

---
+++ PythonCrashCourse/FXTradeLimits.py
@@ -0,0 +1,12 @@
+Intro= "==== Welcome to the trading platform! ===== "
+
+Currency = {
+    "Pound",
+    "Euro",
+    "Dollars"
+}
+
+
+print(Intro)
+while True :
+    trade = float(input("\n Enter FX trade size you want to make in £ "))
