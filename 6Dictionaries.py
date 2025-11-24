aa97bc3120f6889089f0ef93b508a73510ea9873
franckhaba <dufferking10@gmail.com>
2025-11-24 19:23:35 +0000

    adding different functionality to the code to understand it and use them

--- 6Dictionaries.py
+++ 6Dictionaries.py
@@ -1,7 +1,10 @@
 jobOpenings = { 
         
         "shop keeper": 50 ,
-        "Delivery Driver": 90:
-     
-     
-     }
+        "Delivery Driver": 90,
+        "Accountant": 5,
+        "Law": 2,
+        "black man": 100 
+}
+print (jobOpenings)
+del jobOpenings ["black man "]
