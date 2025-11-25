87d01a00976d9e97380110d41c733fe08e47b82b
franckhaba <dufferking10@gmail.com>
2025-11-25 14:28:15 +0000

    added differnet lines like investments and returns" "

--- analyzePortfolio.py
+++ analyzePortfolio.py
@@ -1,1 +1,10 @@
 intro = "==== Welcome to the portfolio analyse ===="
+print (intro)
+
+investments = ["ETF EURO", "Tech fund", "US index", "Real estate","Crypto"]
+
+returns = ["4.5%","-4.1%", "-1.33%", "7%","15%"]
+    
+    
+    
+    
