d17c6417a4b232de289ce7056ff58e559842f913
franckhaba <dufferking10@gmail.com>
2025-11-25 16:14:46 +0000

    added new numbers

--- analyzePortfolio.py
+++ analyzePortfolio.py
@@ -8,10 +8,10 @@ returns = ["4.5%","-4.1%", "-1.33%", "7%","15%"]
 
 performance = { 
                "best": ('Crypto', 15),
-               "worst": ('Tech fund', 15),
-               "AvergReturn": ('Real estate', 15),
-               "aboveAverg": ('US index', 15),
-               "belowAverg": ('ETF Euro', 15),
+               "worst": ('Tech fund', -4.1),
+               "AvergReturn": ('Real estate', 7),
+               "aboveAverg": ('Crypto', 15),
+               "belowAverg": ('ETF Euro', 4.5),
                
                
 }
