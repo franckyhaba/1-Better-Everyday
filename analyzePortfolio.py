f02fc2015e028957d402593f3152d06c518bfa18
franckhaba <dufferking10@gmail.com>
2025-11-25 14:35:34 +0000

    added a dictionaries to the code

--- analyzePortfolio.py
+++ analyzePortfolio.py
@@ -1,10 +1,26 @@
 intro = "==== Welcome to the portfolio analyse ===="
 print (intro)
 
+
 investments = ["ETF EURO", "Tech fund", "US index", "Real estate","Crypto"]
 
 returns = ["4.5%","-4.1%", "-1.33%", "7%","15%"]
+
+performance = { 
+               "best": ('Crypto', 15),
+               "worst": ('Tech fund', 15),
+               "AvergReturn": ('Real estate', 15),
+               "aboveAverg": ('US index', 15),
+               "belowAverg": ('ETF Euro', 15),
+               
+               
+}
+               
     
+    
+    
+    
+
     
     
     
