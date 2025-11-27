1948b840d551b6e66837f6ba9ad3d8f1dc84f5aa
franckhaba <dufferking10@gmail.com>
2025-11-27 16:53:54 +0000

    added while loops but need to clea it up and find the best way to do it

---
+++ PythonCrashCourse/analyzePortfolio.py
@@ -0,0 +1,78 @@
+intro = """
+=== Welcome to the portfolio analyse ===
+\nThis app is to track portfolio information.
+It helps you monitor investments!
+
+\nselect the following number based on what information 
+you want to see 
+\n
+1. Investment in portfolio 
+2. Portfolio return in '%'
+3. Portfolio Performance 
+4. Add new Investment
+5. Exit
+
+"""
+print (intro)
+
+investments = [
+    "ETF EURO",
+    "Tech fund", 
+    "US index", 
+    "Real estate",
+    "Crypto"
+    ]
+
+returns = ["4.5%","-4.1%", "-1.33%", "7%","15%"]
+
+performance = { 
+
+    "best": ('Crypto', 15),
+    "worst": ('Tech fund', -4.1),
+    "avergReturn": ('Real estate', 7),
+    "aboveAverg": ['Crypto', "US index"],
+    "belowAverg": ['ETF Euro','tech fund' ]
+}
+
+
+
+while True : 
+    
+    introList = {
+        1: "Investment in portfolio",
+        2: "Portfolio return in '%'",
+        3: "Portfolio Performance", 
+        4: "Add new Investment",
+        5: "Exit"
+}
+    
+    stage1 = int(input("\n Please select a number from the list 1-4: "))
+    
+    
+    if stage1 not in introList:
+        print(f"\n {stage1} is not in the list try again!!! ")
+        
+        
+    
+        
+    
+    elif stage1 :
+        stage1 == 1
+        print('\n Investment in portfolio was selected' )
+        print(f'\nThe investment in your portfolio are {investments}')
+        
+    elif stage1 :
+        stage1 == 2
+        print('\n Portfolio return was selected' )
+        print(f'\nThe investment in your portfolio are {investments}')
+    
+    
+
+
+    
+    
+    
+
+    
+    
+    
