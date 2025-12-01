f7076a0f87f41efb9da42710534fb989b4147691
franckhaba <dufferking10@gmail.com>
2025-12-01 22:52:14 +0000

    added more notes

--- input.py
+++ input.py
@@ -1,4 +1,17 @@
 #learning about inputs and while loop
+#pause program and wait for users to input the required info
+#using int() 
+#str
+# module % divides numbers  
+#
+# prompt = "\n Tell me something and in will repeat it back to you"
+# prompt = "\n enter  quit to end the program."
+# message = ""
 
-
-#pause program and wait for users to input the required info 
+# while message != 'quit'
+#   message = input(prompt)
+#   print (message)
+#  
+# I need how to use operator 
+#
+#
