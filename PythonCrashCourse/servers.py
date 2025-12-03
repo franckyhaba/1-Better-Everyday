7380cdbed5c8284df73b9f93e22d397b1c4f986f
franckhaba <dufferking10@gmail.com>
2025-12-03 07:42:10 +0000

    new program that uses functions to get a hang of it'

---
+++ PythonCrashCourse/servers.py
@@ -0,0 +1,23 @@
+# Write a Python script that:
+# Reads a list of server names from a file called servers.txt.
+# Pings each server once.
+# Records the result (UP or DOWN) into a log file named healthcheck.log.
+# Includes a function to format the log output.
+# Uses a loop, lists, functions, and error handling.
+# Prints a short summary at the end showing:
+# total servers checked
+# how many were up
+# how many were down
+
+
+
+
+serversName = {
+    
+    'art':  'up', 
+    'Aws': 'Down',
+    'Hubspot': 'Down',
+        
+    
+    
+}
