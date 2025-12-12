ab6ba92b61fc06a15c710c9e97de33c12806200c
franckhaba <dufferking10@gmail.com>
2025-12-12 22:27:59 +0000

    added new informations

--- PythonCrashCourse/usingFunctions.py
+++ PythonCrashCourse/usingFunctions.py
@@ -14,4 +14,18 @@ KEYWORD ARGUMENTS
 
  stops things like 
  jon named pig
+ 
+    |
+    |
+    |
+    |
+    V
 '''
+
+
+def describeCars( type, name):
+    
+    """" Display information on pets """
+    print(f"\nI have a {type}.")
+    print(f"\nmy {type} name is {name}.")
+describeCars( type = 'astra',name ='jon')
