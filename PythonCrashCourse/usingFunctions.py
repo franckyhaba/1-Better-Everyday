2bb6c4edf1532480ae1d3e4f7fddf9c4825200c7
franckhaba <dufferking10@gmail.com>
2025-12-12 22:18:03 +0000

    added more informations

--- PythonCrashCourse/usingFunctions.py
+++ PythonCrashCourse/usingFunctions.py
@@ -3,4 +3,4 @@ def describePets( type, name):
     """" Display information on pets """
     print(f"\nI have a {type}.")
     print(f"\nmy {type} name is {name}.")
-describePets('pig, jon')
+describePets('pig','jon')
