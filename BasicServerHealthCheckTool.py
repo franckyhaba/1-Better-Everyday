5f3814b3950427f9a08fd5dbf9bfabc892c0cf1a
franckhaba <dufferking10@gmail.com>
2025-12-15 21:37:44 +0000

    added new program

---
+++ BasicServerHealthCheckTool.py
@@ -0,0 +1,19 @@
+ServersStatus =  {
+
+"Web01" : "UP",
+"DBO1" :  "Down",
+"ETH01"  : "UP",
+"AIR01"   : "Down",
+"FRA01 "   :    "Down",
+"BIC01"    : "UP"
+
+}
+
+
+def healthCheck():
+    
+    
+
+
+    
+    
