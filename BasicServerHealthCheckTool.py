e997ad874b49b08fde1bb793f41a54f7c0840025
franckhaba <dufferking10@gmail.com>
2025-12-16 22:42:37 +0000

    built up the function

--- BasicServerHealthCheckTool.py
+++ BasicServerHealthCheckTool.py
@@ -1,6 +1,6 @@
 serversStatus =  {
 
-"Web01" : "UP",
+"WEB01" : "UP",
 "DBO01" :  "Down",
 "ETH01"  : "UP",
 "AIR01"   : "Down",
@@ -10,22 +10,31 @@ serversStatus =  {
 }
 
 while True: 
-    info = input('Please enter what servers to check: ')
+    info = input('\nPlease enter what servers to check: ')
     info = info.upper()
     ''''Can write the above code like or just put it at the end .upper()'''
     if info in serversStatus:
         print(f"\nThe server {info} has been located! ")
-        
+        break
     else:
         print(f"\nThe server {info} cant be located try again. ")
 
 def healthCheck(verbose):
+    
+    serverCheck = input("\nDo want to check the server health? please enter 'Y\N'").upper()
+    
+    if  serverCheck == 'Y':
+        print(f"The {info} health check came back {status}")
 
-    if verbose == True:
-        print ("detailed out")
+    
     else:
-        verbose == False
-        print("minimal output")
+        print(f"The {info} health check wasn't preformed")
+        
+        
+        
+    for server, status in  serversStatus.item():
+        print(f"The {server} health check came back {status}")
+        
 
 
     healthCheck()
