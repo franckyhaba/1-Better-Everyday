6df4da56dbb04c0861cdd37804ba582652882dd2
franckhaba <dufferking10@gmail.com>
2025-12-16 22:15:49 +0000

    added more to the code such as upper and comments

--- BasicServerHealthCheckTool.py
+++ BasicServerHealthCheckTool.py
@@ -1,19 +1,21 @@
 serversStatus =  {
 
 "Web01" : "UP",
-"DBO1" :  "Down",
+"DBO01" :  "Down",
 "ETH01"  : "UP",
 "AIR01"   : "Down",
-"FRA01 "   :    "Down",
+"FRA01"   :    "Down",
 "BIC01"    : "UP"
 
 }
 
 while True: 
     info = input('Please enter what servers to check: ')
-    
+    info = info.upper()
+    ''''Can write the above code like or just put it at the end .upper()'''
     if info in serversStatus:
         print(f"\nThe server {info} has been located! ")
+        
     else:
         print(f"\nThe server {info} cant be located try again. ")
 
