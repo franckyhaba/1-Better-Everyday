62eaf7b5cbc568c50506e2305ad231b13d588a7b
franckhaba <dufferking10@gmail.com>
2026-01-28 11:58:23 +0000

    added more code and works well need to add error excepttions

--- PythonCrashCourse/Overpayment.py
+++ PythonCrashCourse/Overpayment.py
@@ -1,4 +1,4 @@
-invoice =  "10000"
+invoice =  float("10000")
 
 intro = "\nThis system is to pay for your invoice."
 print(intro)
@@ -6,5 +6,17 @@ payment = float(input("\nEnter the amount the amount you want to pay: "))
 print (payment)
 
 if payment == invoice:
-    print ("Your payment of {payment} has paid of the invoice")
+    print (f"\nYour payment of ${payment} has covered all of the invoice")
+    
+elif payment > invoice:
+    
+    calOverPayment = invoice - payment 
+    
+    print (f"\nThe ${payment} of is an overpayment, you a due a return of  ${calOverPayment}")
+    
+else :
+    payment < invoice
+    CalUnderPayment = invoice - payment 
+    
+    print (f"\nThe ${payment} of is an underpayment, you still have an outstanding balance of ${CalUnderPayment}")
     
