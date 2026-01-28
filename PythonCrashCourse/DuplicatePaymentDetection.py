8073f3e3d72cddd35f36c8bfd295c32928f3ede0
franckhaba <dufferking10@gmail.com>
2026-01-28 22:45:25 +0000

    doing check for duplicates eg

---
+++ PythonCrashCourse/DuplicatePaymentDetection.py
@@ -0,0 +1,22 @@
+invoices = {
+    "BNP FP": 5000,
+    "LVMH FP": 2500,
+    "MC FP": 70000,
+    "RNO FP": 16000,
+    "BAOC FP":4000,
+    "BNP FP": 5000  
+}
+
+
+
+def detectDuplicatePayments(invoice, payments):
+    
+    if invoice 
+        return 
+    else:
+        print("Zero duplicates can be found")
+        
+
+while True: 
+    Intro = str(input("Do you want to check for Duplicates in Data if yes enter 'y' else 'N' to exit: "))
+    
