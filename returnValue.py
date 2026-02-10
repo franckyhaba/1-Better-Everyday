80e806e792b9643bd5db2780ae2f7de2dc66e79d
franckhaba <dufferking10@gmail.com>
2026-02-10 08:06:50 +0000

    added optinoal middle name by using mName =''

--- returnValue.py
+++ returnValue.py
@@ -1,7 +1,11 @@
-def getFullName(fName,mName ,sName):
-    
-    fullName = f"\n {fName} {mName}{sName}"
+def getFullName(fName,sName, mName =''):
+    if mName:
+        fullName = f"\n {fName} {mName}{sName}"
+    else:
+        fullName = f"\n {fName} {sName}"
     return fullName.title()
 
 footy = getFullName('jimi','jon','Mac')
+print(footy)
+footy = getFullName('jimi','Mac')
 print(footy)
